0460124873fb3228cd27444f8bf757161502a018
Coderj1 <ramseslove52@gmail.com>
2025-09-05 22:26:54 +0100

    update site

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Services from './pages/Services';
+import './App.css'
 
 function App() {
   return (
